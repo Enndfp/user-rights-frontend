1359d499a1c131883a6a0ba2334d330522a46abb
Enndfp <3193947435@qq.com>
2023-07-11 21:24:37 +0800

    add filing information

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -4,9 +4,19 @@ import {CSDN_LINK, GITEE_LINK, GITHUB_LINK} from "@/constant";
 const Footer: React.FC = () => {
   const defaultMessage = 'Enndfp出品';
   const currentYear = new Date().getFullYear();
+  const beian = '宁ICP备2023000742号-1';
+  const beianUrl = 'https://beian.miit.gov.cn';
   return (
     <DefaultFooter
-      copyright={`${currentYear} ${defaultMessage}`}
+      // @ts-ignore
+      copyright={
+        <>
+          {currentYear} {defaultMessage} |{' '}
+          <a href={beianUrl} target="_blank" rel="noreferrer">
+            {beian}
+          </a>
+        </>
+      }
       links={[
         {
           key: 'csdn',
